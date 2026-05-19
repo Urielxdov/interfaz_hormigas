200aeb35e088b1030839b5f89f3b16397e3ab2eb
urielxdov <urieledgar878@gmail.com>
2026-05-19 14:51:27 -0600

    chore(ui): install Inter font and configure tailwind fontFamily

--- apps/mobile/hormigas_mobile/tailwind.config.js
+++ apps/mobile/hormigas_mobile/tailwind.config.js
@@ -7,7 +7,14 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter_400Regular'],
+        'sans-medium': ['Inter_500Medium'],
+        'sans-semibold': ['Inter_600SemiBold'],
+        'sans-bold': ['Inter_700Bold'],
+      },
+    },
   },
   plugins: [],
 }
